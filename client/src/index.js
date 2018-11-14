77db58491a6a31de425a0582d2a96a7c0646cc0c
Yamkela Venfolo <yamkela.venfolo@SAMAC137-2.local>
2018-11-14 21:23:52 +0200

    Weather

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './views/css/styles.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import registerServiceWorker from './registerServiceWorker';
 
-// Store
-import store from './store/index';
+//Views
+import App from './views/Components/App';
+import Home from './views/Containers/Home';
+import CurrentWeather from './views/Containers/CurrentWeather';
+import ErrorDisplay from './views/Components/ErrorDisplay';
 
-// Views
-import { App, ErrorDisplay } from './views/Components/index';
-import { Home, CurrentWeather } from './views/Containers/index';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App>
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/current-weather' component={CurrentWeather}/>
-        <Route exact path='/error' component={ErrorDisplay}/>
-      </App>
-    </Router>
-  </Provider>
-  , document.getElementById('root'));
+	<Router>
+	  <App>
+		  <Route exact path='/' component={Home}/>
+		  <Route exact path='/current-weather' component={CurrentWeather}/>
+		  <Route exact path='/error' component={ErrorDisplay}/>
+	  </App>
+	</Router>
 
-// registerServiceWorker(); // eslint-disable-line no-undef
+,document.getElementById('root'));
+
+// registerServiceWorker();
